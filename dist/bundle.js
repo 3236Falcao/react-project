c5cf33e88f6b4f0099492417206da3f7f57859eb
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-09 08:26:39 -0300

    Criação do bundle.js

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$address;
+
+var user = {
+  name: 'Daniel'
+};
+console.log((_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street); //Não consegui converter para o babel. Parei em configurando o ambinete 9:31
